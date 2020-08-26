427f52c4026f6d43907a6d95e50d4862e7e0c9fd
bharathjinka09 <bharathjinka09@gmail.com>
2020-08-26 15:17:52 +0530

    Updates

---
+++ precache-manifest.7a4287f9bd0396b686c126c3796dd44a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da248015590fd9bd53bbc52688abe9ed",
+    "url": "/react-registration-form/index.html"
+  },
+  {
+    "revision": "ec0c2504c83e107e3f3f",
+    "url": "/react-registration-form/static/css/main.10078b94.chunk.css"
+  },
+  {
+    "revision": "6a3bfa477a4866f75c91",
+    "url": "/react-registration-form/static/js/2.e06aa272.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-registration-form/static/js/2.e06aa272.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec0c2504c83e107e3f3f",
+    "url": "/react-registration-form/static/js/main.751a4cee.chunk.js"
+  },
+  {
+    "revision": "172d65e2b6848b13af49",
+    "url": "/react-registration-form/static/js/runtime-main.9b729f64.js"
+  }
+]);
